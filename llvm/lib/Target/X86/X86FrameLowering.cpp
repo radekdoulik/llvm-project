ec1861d1b6bac599d85be2fd4f5bb38a65c3b801
Jo Shields <directhex@apebox.org>
2023-06-15 18:13:38 -0400

    Add support for emitting mono specific EH tables (#430)
    * Add support for emitting mono specific EH tables. These tables can be used to map a mono method index to its associated EH and unwind info.
    
    (cherry picked from commit 3b5d2380563fef452c649b4dacf3eb1b66b0bc8b)
    
    * Add a flag to disable emission of .llvm_faultmaps metadata.
    
    (cherry picked from commit 3c78a53aa9b2602bfab501fa3c81fae1d4a1412a)
    
    * Fix llvm-as to correctly parse monocc.
    
    (cherry picked from commit 3606e10dffb8d2cce1ca4cb1ccd562d17e4536aa)
    
    * Fixes Mono LLVM 6.0 for Windows x64.
    
    * Adjust mono's LLVM calling convention to use win64 calling convention.
    * Emit both Windows native unwind info + Dwarf unwind info for mono methods.
    * Don't emit native personality handlers for mono methods.
    * Handle Mono and Win64 calling convention similarities when needed.
    
    (cherry picked from commit 5cdcbd4158e5193028c00a438af77cfd7d239eab)
    
    * Add a mono calling convention which allows the passing of an extra argument in a platform specific non-abi register. This is used to implement the passing of rgctx/imt arguments in mono.
    
    (cherry picked from commit d4bb6952b7dea56b21cdd8f1ab8295517b0cdd65)
    
    * Use LLVM_BUILTIN_TRAP macro, not __builtin_trap(), fixes MSVC
    
    (cherry picked from commit 03dec339906c2470b78f558f33eb9b5ef58bd51f)
    
    * LLVM 16 changes to MonoException.cpp
    
    * Fix WinException error
    
    * Fix MonoException errors
    
    ---------
    
    Co-authored-by: Zoltan Varga <vargaz@gmail.com>
    Co-authored-by: Imran Hameed <imhameed@microsoft.com>
    Co-authored-by: lateralusX <lateralusx.github@gmail.com>

--- llvm/lib/Target/X86/X86FrameLowering.cpp
+++ llvm/lib/Target/X86/X86FrameLowering.cpp
@@ -1556,7 +1556,10 @@ void X86FrameLowering::emitPrologue(MachineFunction &MF,
   bool NeedsWinFPO =
       !IsFunclet && STI.isTargetWin32() && MMI.getModule()->getCodeViewFlag();
   bool NeedsWinCFI = NeedsWin64CFI || NeedsWinFPO;
-  bool NeedsDwarfCFI = needsDwarfCFI(MF);
+  bool NeedsDwarfCFI =
+      Fn.getCallingConv() == CallingConv::Mono
+          ? MF.needsFrameMoves()
+          : needsDwarfCFI(MF);
   Register FramePtr = TRI->getFrameRegister(MF);
   const Register MachineFramePtr =
       STI.isTarget64BitILP32() ? Register(getX86SubSuperRegister(FramePtr, 64))
